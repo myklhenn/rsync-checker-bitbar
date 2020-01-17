2a7639552d3f274edf60d264c5f242cc8f59aadf
Michael Henning <mykl951@gmail.com>
2020-01-16 16:38:12 -0800

    Added config file support - both scripts now look for a config file at ~/.rsync-checker-config

--- server-command.sh
+++ server-command.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # This script, intended for use in conjunction with the "rsync-checker" BitBar
-# script, utilizes the environment variables described below to facilitate
-# checking backup log files and starting backup scripts for the configured
-# client and server.
+# script, utilizes the config variables (found either in the shell's
+# environment or as sourced from the config file ~/.rsync-checker-config)
+# described below to facilitate checking backup log files and starting backup
+# scripts for the configured client and server.
 #
 # `./server-command.sh --view-log` establishes an SSH connection and shows the
 # most recent contents of the configured log file.
@@ -11,13 +12,15 @@
 # `./server-command.sh --start-backup` establishes an SSH connection and
 # executes the configured backup script.
 #
-# Expected environment variables:
+# Expected config variables:
 #
 # BACKUP_SERVER_HOSTNAME -- hostname of backup server (as used in ssh config)
 # BACKUP_SCRIPT_PATH -- path on server of backup script
 # BACKUP_LOG_FILE_PATH -- path on server of backup log file
 # BACKUP_SSH_KEY_PATH -- local path of private SSH key used to connect to server
 # BACKUP_SSH_KEY_COMMENT -- comment of private key to check for in SSH keychain
+
+[ -e $HOME/.rsync-checker-config ] && source $HOME/.rsync-checker-config
 
 if ! ssh-add -l | grep "$BACKUP_SSH_KEY_COMMENT" > /dev/null; then
   echo "Adding your server's SSH key to your agent first..."
